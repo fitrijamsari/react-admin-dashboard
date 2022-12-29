7c3966fb0a86447a7699290d0380edf5ce654e14
fitrijamsari <fitrijamsari@gmail.com>
2022-12-29 13:10:15 +0800

    update uiux sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 function Sidebar() {
   return (
@@ -9,17 +21,58 @@ function Sidebar() {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
-            <span>Dashbaord</span>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashbaord</span>
+            <AddBusinessIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashbaord</span>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
           </li>
           <li>
-            <span>Dashbaord</span>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <AnalyticsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <LocalHospitalIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
